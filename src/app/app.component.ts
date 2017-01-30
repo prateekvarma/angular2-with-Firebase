93726860b9a279b9b2c3f447b28fce79db7663e8
Prateek Varma <prtk.varma@gmail.com>
2017-01-30 16:19:02 +0000

    first load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  
+  constructor() {
+      // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCnXVQq2E4lMGz9-xK1aggcA_MZwUGwGXU",
+    authDomain: "scorching-fire-5416.firebaseapp.com",
+    databaseURL: "https://scorching-fire-5416.firebaseio.com",
+    storageBucket: "scorching-fire-5416.appspot.com",
+    messagingSenderId: "558764680726"
+  };
+  initializeApp(config);
+  
+  var root = database().ref('messages');
+  root.on('value', function(snap) {
+    console.log(snap.val());
+  });
+  
+  }
+  
 }
